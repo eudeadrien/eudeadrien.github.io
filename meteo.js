0c31c6a3b28f349f39a546a72ae5fa11e3615433
eudeadrien <105950292+eudeadrien@users.noreply.github.com>
2022-07-13 14:08:16 +0200

    Add files via upload

--- meteo.js
+++ meteo.js
@@ -1,17 +1,17 @@
 var callback = function(data) {
 	console.log("données api", data)
 	var element = document.getElementById("zone_meteo");
-	var icon = document.getElementById("icon_meteo");
-	let img = new Image();
-	img.src = 
-	element.innerHTML = "Il faisait " + data.current.temperature + " °C à " + data.location.name + " vers " + data.current.observation_time ;
+	//var img =  document.getElementById("icon_meteo");
+	//img.src = data.forecast.weather_icons
+	element.innerHTML = "Il annonce entre " + data.forecast.tmax + " °C et " + data.forecast.tmin + " °C à " + data.city.name + " aujoud'hui " ;
+	//img.innerHTMl = img ;
 }
 
 
 function buttonClickGET() {
 	var queryLoc = document.getElementById("queryLoc").value;
-	var url ="http://api.weatherstack.com/current?access_key=d6d0044e3a1d345592e6ed9c4a27b331&query="+queryLoc;
-
+	var url ="https://api.meteo-concept.com/api/forecast/daily/0?token=6dfdb2eb41259173263c8ecdf0910467b361773d3ae9ce4f091d10ee2334a3cb&insee="+queryLoc;
+/*http://api.weatherstack.com/current?access_key=d6d0044e3a1d345592e6ed9c4a27b331&query="+queryLoc;*/
 	$.get(url, callback).done(function() {
 	})
 	.fail(function() {
