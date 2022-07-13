da03076541687b3d51f4bf12823c28f38be57402
eudeadrien <105950292+eudeadrien@users.noreply.github.com>
2022-07-13 15:04:45 +0200

    Add files via upload

--- meteo.js
+++ meteo.js
@@ -3,15 +3,16 @@ var callback = function(data) {
 	var element = document.getElementById("zone_meteo");
 	//var img =  document.getElementById("icon_meteo");
 	//img.src = data.forecast.weather_icons
-	element.innerHTML = "Il annonce entre " + data.forecast.tmax + " °C et " + data.forecast.tmin + " °C à " + data.city.name + " aujoud'hui " ;
+	temperature = Math.floor(data.main.temp - 273);
+	element.innerHTML = "Il fait actuellement " + temperature + " °C à " + data.name;
 	//img.innerHTMl = img ;
 }
 
 
 function buttonClickGET() {
 	var queryLoc = document.getElementById("queryLoc").value;
-	var url ="https://api.meteo-concept.com/api/forecast/daily/0?token=6dfdb2eb41259173263c8ecdf0910467b361773d3ae9ce4f091d10ee2334a3cb&insee="+queryLoc;
-/*http://api.weatherstack.com/current?access_key=d6d0044e3a1d345592e6ed9c4a27b331&query="+queryLoc;*/
+	var url ="https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=e28f76918fe3207c5bba56d93835d922&lang=fr";
+	
 	$.get(url, callback).done(function() {
 	})
 	.fail(function() {
@@ -21,3 +22,9 @@ function buttonClickGET() {
 		//alert( "finished" );
 	});
 }
+
+
+
+/*https://api.meteo-concept.com/
+http://api.weatherstack.com/current?access_key=d6d0044e3a1d345592e6ed9c4a27b331&query="+queryLoc;
+https://api.meteo-concept.com/api/forecast/daily/0?token=6dfdb2eb41259173263c8ecdf0910467b361773d3ae9ce4f091d10ee2334a3cb&insee="+queryLoc */
